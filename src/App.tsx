7b300d6bac800bd97a59c4b8a3917f7e2e6d0fb4
al3xisvg <vasquezgarciaalexis@gmail.com>
2024-01-06 13:37:07 -0500

    [update] set poppins

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <a className="text-3xl font-bold underline bg-red-500">Otro boton</a>
     </>
   )
 }
