81fabeef8af3fb08e9fc56329af68c9565f4a4f8
al3xisvg <vasquezgarciaalexis@gmail.com>
2024-01-06 18:42:35 -0500

    [udpate] complete form

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,111 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import mailIcon from './assets/mail.svg'
+import lockIcon from './assets/lock.svg'
+import unlockIcon from './assets/unlock.svg'
+
+import logo from '/logo.png'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [isVisible, setVisible] = useState<boolean>(false)
+
+  const inputText = (label: string, name: string, icon: string) => {
+    return (
+      <div className="flex flex-col items-start">
+        <label htmlFor="email">{label}</label>
+
+        <div className="flex bg-slate-900 justify-between w-full">
+          <input
+            type="text"
+            id={name}
+            name={name}
+            placeholder={label}
+            className="w-full focus:outline-none px-2 text-sm"
+          />
+          <div className="bg-green-500 rounded p-2">
+            <img src={icon} className="w-8 h-8 !fill-slate-50 !stroke-slate-50 !text-slate-50" />
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  const inputPassword = (label: string, name: string) => {
+    return (
+      <div className="flex flex-col items-start">
+        <label htmlFor="email">{label}</label>
+
+        <div className="flex bg-slate-900 justify-between w-full">
+          <input
+            type={isVisible ? 'text' : 'password'}
+            id={name}
+            name={name}
+            placeholder={label}
+            className="w-full focus:outline-none px-2 text-sm"
+          />
+          <div className="bg-green-500 rounded p-2" onClick={(_) => setVisible(!isVisible)}>
+            <img
+              src={isVisible ? unlockIcon : lockIcon}
+              className="w-8 h-8 !fill-slate-50 !stroke-slate-50 !text-slate-50"
+            />
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  const separator = (text: string) => {
+    return (
+      <div className="flex w-full justify-between items-center">
+        <hr className="w-2/5" />
+        <span>{text}</span>
+        <hr className="w-2/5" />
+      </div>
+    )
+  }
+
+  const header = (
+    <div className="flex flex-col">
+      <img src={logo} className="h-16 pb-4" />
+      <span className="text-xl font-bold">I.E. 1258 Sebasti&aacute;n Lorente</span>
+      <span className="text-md">Portal de docentes</span>
+    </div>
+  )
+
+  const form = (
+    <form className="flex flex-col py-8">
+      {inputText('Correo electrónico', 'email', mailIcon)}
+      {inputPassword('Contraseña', 'password')}
+
+      <div className="flex justify-end py-4">
+        <a href="#" className="underline text-sm">
+          ¿Olvidó su contrase&ntilde;a?
+        </a>
+      </div>
+
+      <button className="bg-green-500 border-2 border-green-500 rounded-md h-10 font-bold">
+        Ingresar
+      </button>
+
+      {separator('ó')}
+
+      <button className="border-2 border-green-500 text-green-500 rounded-md h-10 font-bold">
+        Registrarse
+      </button>
+    </form>
+  )
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex h-screen">
+      <div className="w-full flex justify-center items-center">
+        <div className="w-10/12">
+          {header}
+          {form}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-      <a className="text-3xl font-bold underline bg-red-500">Otro boton</a>
-    </>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {},
+      backgroundImage: {
+        banner: "url('./assets/banner.png')",
+      },
+    },
   },
   plugins: [],
 }
