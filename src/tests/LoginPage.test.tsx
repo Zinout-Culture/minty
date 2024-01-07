9037095de2394d9e5624a56b676b1a3a8e6798ec
al3xisvg <vasquezgarciaalexis@gmail.com>
2024-01-07 09:08:52 -0500

    [update] set first tests

--- src/tests/LoginPage.test.tsx
+++ src/tests/LoginPage.test.tsx
@@ -5,13 +5,23 @@ import { render, screen } from '@testing-library/react'
 import LoginPage from '../pages/LoginPage'
 
 describe('Login Page', () => {
-  test('Renderizado', () => {
+  beforeEach(() => {
     render(
       <Router>
         <LoginPage />
       </Router>,
     )
+  })
 
+  test('Título', () => {
+    expect(screen.getByText('Portal de docentes')).toBeDefined()
+  })
+
+  test('Botón Ingresar', () => {
     expect(screen.getByText('Ingresar')).toBeDefined()
+  })
+
+  test('Botón Registrarse', () => {
+    expect(screen.getByText('Registrarse')).toBeDefined()
   })
 })
